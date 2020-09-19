e0b2cea7c8e385ff26d7a44e7e3cf427baca86e1
AldoLeonMarquez <70210607+AldoLeonMarquez@users.noreply.github.com>
2020-09-18 23:38:23 -0500

    Update 20200916020952_create_books.rb

--- db/migrate/20200916020952_create_books.rb
+++ db/migrate/20200916020952_create_books.rb
@@ -5,7 +5,7 @@ class CreateBooks < ActiveRecord::Migration[6.0]
       t.string "author"
       t.string "genre"
       t.integer "price"
-      t.string "published-date"
+      t.string "publisheddate"
       t.timestamps
     end
   end
